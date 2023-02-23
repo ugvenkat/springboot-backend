6b4f1e7b237f2f7d3fadba22782ec21a0d323735
Venkat Gandhi <ugvenkat@hotmail.com>
2023-02-23 07:51:00 -0600

    second commit

---
+++ src/main/java/learn/springboot/backend/repository/EmployeeRepository.java
@@ -0,0 +1,10 @@
+package learn.springboot.backend.repository;
+
+import learn.springboot.backend.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long>{
+
+}
